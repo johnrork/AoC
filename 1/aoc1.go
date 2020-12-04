5140a28a6849831f4d02633befc56afaab41a038
johnrork <johnr@aluminumtrailer.com>
2020-12-03 21:30:09 -0600

    Fix formatting

--- 1/aoc1.go
+++ 1/aoc1.go
@@ -1,24 +1,25 @@
 package main
+
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
 	"strconv"
 )
 
 func contains(s []int, e int) bool {
-    for _, a := range s {
-        if a == e {
-            return true
-        }
-    }
-    return false
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
 
 func multiply_two_numbers_totaling(val int, list []int) int {
-	for _, v := range(list) {
+	for _, v := range list {
 		diff := val - v
-		if (contains(list, diff)){
+		if contains(list, diff) {
 			return diff * v
 		}
 	}
@@ -26,23 +27,23 @@ func multiply_two_numbers_totaling(val int, list []int) int {
 }
 
 func multiply_three_numbers_totaling(val int, list []int) int {
-	for _, v := range(list){
+	for _, v := range list {
 		diff := val - v
 		match := multiply_two_numbers_totaling(diff, list)
-		if (match != 0){
+		if match != 0 {
 			return v * match
 		}
 	}
 	return 0
 }
 
-func main(){
+func main() {
 	f, _ := os.Open("expenses.txt")
 	defer f.Close()
 
 	var list []int
-    scanner := bufio.NewScanner(f)
-    for scanner.Scan(){
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
 		val, _ := strconv.Atoi(scanner.Text())
 		list = append(list, val)
 	}
